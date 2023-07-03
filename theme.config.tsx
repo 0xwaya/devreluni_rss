883515d84f235605118616f9cbb843d2a3d0b557
e.mercado <admin@wayalabs.com>
2023-07-03 11:24:40 -0400

    update initial content to main

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from 'next/image'
+
+import druLogo from './.github/dru_logo.png'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <Image
+      src={druLogo}
+      alt="DevRel University Logo"
+      width={50}
+      height={50}
+    />
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/0xwaya/DRU_Resources',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/TcAC7RVB7H',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/0xwaya/DRU_Resources',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'DevRel University Resources',
   },
 }
 
